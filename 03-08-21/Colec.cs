53bb6d271ead50214a1b4fe80d8be3dc60a2f23d
KiranChandran22 <88082915+KiranChandran22@users.noreply.github.com>
2021-08-03 17:22:36 +0530

    Add files via upload

---
+++ 03-08-21/Colec.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace ConsoleApp1
+{
+    class Colec
+    {
+
+
+        static void Main(string[] args)
+        {
+            ArrayList al = new ArrayList();
+            al.Add(" WHITE CHOCOLATE RASPBERRY CHEESECAKE");
+            al.Add(" BAKLAVA");
+            al.Add("LEMON CAKE DOBERGE");
+            al.Add(" MOLTEN CHOCOLATE LAVA CAKE");
+            al.Add("CHOCOLATE FONDUE");
+            FileStream fs = new FileStream("D:\\Inatech\\03-08-21\\Fyle1.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamWriter swr = new StreamWriter(fs);
+            foreach (var i in al)
+                swr.Write(i);
+            swr.Flush();
+            swr.Close();
+            fs.Close();
+
+            FileStream fr = new FileStream("D:\\Inatech\\03-08-21\\Fyle1.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fr);
+            sr.BaseStream.Seek(0, SeekOrigin.Begin);
+            Console.WriteLine(
+                sr.ReadToEnd());
+            sr.Close();
+            fr.Close();
+            FileInfo f1 = new FileInfo("D:\\Inatech\\03-08-21\\Fyle1.txt");
+
+            Console.WriteLine(f1.Length);
+            Console.WriteLine(f1.CreationTime);
+            Console.Read();
+        }
+
+
+
+    }
+
+}
---
+++ 03-08-21/seria.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+
+namespace ConsoleApp1
+{
+    [Serializable]
+    class Seria
+    {
+        int id;
+        String name;
+        String gender;
+        int age;
+        public Seria(int id, String name, String gender, int age)
+        {
+            this.name = name;
+            this.id = id;
+            this.gender = gender;
+            this.age = age;
+        }
+        
+        
+        public static void Main()
+        {
+            Seria sclass = new Seria(24, "Kiran Chandran", "male", 21);
+
+            FileStream fs = new FileStream("D:\\Inatech\\03-08-21\\Serial.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fs, sclass);
+            fs.Close();
+
+
+            FileStream fr = new FileStream("D:\\Inatech\\03-08-21\\Serial.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryFormatter br = new BinaryFormatter();
+
+            Seria sc = (Seria)br.Deserialize(fr);
+            Console.WriteLine(sc.name);
+            Console.WriteLine(sc.age);
+            Console.WriteLine(sc.gender);
+            Console.WriteLine(sc.id);
+            fs.Close();
+            Console.Read();
+        }
+    }
+}
