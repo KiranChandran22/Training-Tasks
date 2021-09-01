dea3a9308be416bc8448d9ec9233afc3a1b25e14
KiranChandran22 <88082915+KiranChandran22@users.noreply.github.com>
2021-09-01 14:46:57 +0530

    Add files via upload

---
+++ Project/PizzeriaController.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project_A.Models;
+
+namespace Project_A.Controllers
+{
+    public class PizzeriaController : Controller
+    {
+        // GET: Project
+        public ActionResult Index()
+        {
+            return View("Home");
+        }
+        public ActionResult VegPizza(string action)
+        {
+            if (action == "VEG")
+            {
+                projector pobj = new projector();
+                DataTable dt = pobj.GetVegpizza();
+                return View("VegPizza", dt);
+            }
+            if (action == "NON VEG")
+            {
+                projector pobj = new projector();
+                DataTable dt = pobj.GetNoNveg();
+                return View("VegPizza", dt);
+            }
+            return View("Index");
+        }
+        public ActionResult order()
+        {
+            projector pobj = new projector();
+           DataTable dt= pobj.getfullpizza();
+            return View("order",dt);
+        }
+        public ActionResult Crate(string Pizza_name)
+        {
+            projector pobj = new projector();
+            DataTable dt = new DataTable(); 
+            dt=pobj.PizzabyName(Pizza_name);
+            return View("Crate",dt);
+        }
+        public ActionResult Add_to_crate(FormCollection frm, string action)
+        {
+            if (action == "Check Crate")
+            {
+                string Pizza_name = frm["Pizza_name"];
+                int Pizza_id = Convert.ToInt32(frm["Pizza_id"]);
+                int quantity = Convert.ToInt32(frm["Quantity"]);
+                string crust = frm["crust"];
+                string toppings = frm["Toppings"];
+                string size = frm["size"];
+                int price = Convert.ToInt32(frm["price"]);
+                projector pobj = new projector();
+                pobj.insert_into_order(Pizza_name, Pizza_id, quantity, crust, toppings,size,price);
+                DataTable dt = new DataTable();
+                dt = pobj.Getorder();
+                return View("Cratefinal",dt);
+            }
+            if (action == "Cancel")
+            {
+                return View("Home");
+            }
+            return View();
+        }
+        //public ActionResult Add_to_crate(FormCollection frm,string action)
+        //{
+
+        //    return View();
+        //}
+        public ActionResult Cratefinal()
+        {
+            projector pobj = new projector();
+            DataTable dt = new DataTable();
+           dt = pobj.Getorder();
+           return View("Cratefinal", dt);
+        }
+        public ActionResult delete(int Pizza_id)
+        {
+            projector dmoj = new projector();
+            dmoj.delete(Pizza_id);
+            return RedirectToAction("Cratefinal");
+            
+        }
+        public ActionResult Login(string Action)
+        {
+            projector pobj = new projector();
+            DataTable dt = new DataTable();
+            dt = pobj.gettable();
+            if (Action== "Order Now")
+            {
+                RedirectToAction("Login");
+            }
+            if (Action== "Continue Shopping")
+            {
+                return View("Home");
+            }
+            return View(dt);
+        }
+        public ActionResult UpdateRecord(FormCollection frm, string action)
+        {
+            if (action == "Log In")
+            {
+                int order_id = Convert.ToInt32(frm["txtid"]);
+                string name = frm["txtmname"];
+                string address = frm["txtaddress"];
+                string phno = frm["txtphno"];
+                projector pobj = new projector();
+                pobj.update(order_id, name, address, phno);
+                DataTable dt = new DataTable();
+                dt = pobj.Getorderwithquanprice();
+                ViewData["tot"] = Convert.ToInt32(dt.Compute("SUM(price)", string.Empty));
+                return View("ordersummary",dt);
+            }
+            else
+            {
+                return View("Home");
+            }
+        }
+        public ActionResult ordersummary()
+        {
+            return View();
+        }
+        public ActionResult addtocratetab(string action)
+        {
+            ViewBag.Message = "Once paid money isn't refundable";
+            if (action == "Order Now")
+            {
+                projector pobj = new projector();
+                ViewBag.Message = "Once paid money isn't refundable";
+                pobj.addtocrate();
+                pobj.gettablecrate();
+                DataTable dt = new DataTable();
+                dt = pobj.Getorderwithquanprice();
+                ViewData["tot"] = Convert.ToInt32(dt.Compute("SUM(price)", string.Empty));
+                return View("Creditcard",dt);
+            }
+            else
+            {
+                return View("Home");
+            }
+        }
+        public ActionResult Creditcard(string action)
+        {
+            if(action== "Pay")
+            { 
+                projector pobj = new projector();
+                DataTable dt = new DataTable();
+                pobj.addtocrate();
+                dt = pobj.gettablecrate();
+                pobj.truncate();
+                return View("orderstatus",dt);
+            }
+            return View("Home");
+        }
+        public ActionResult orderhistory()
+        {
+            
+            return View();
+        }
+        public ActionResult orderhistable(FormCollection frm,string action)
+        {
+            if (action == "Search")
+            {
+                projector pobj = new projector();
+                DataTable dt = new DataTable();
+                string name = frm["txtmname"];
+                dt = pobj.getorderhistory(name);
+                return View("orderhistable", dt);
+            }
+            return View();
+        }
+       public ActionResult about()
+        {
+            return View();
+        }
+    }
+}
---
+++ Project/projector.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Project_A.Models
+{
+    public class projector
+    {
+        SqlConnection con;
+        public projector()
+        {
+            con = new SqlConnection("Data Source=LAPTOP-D9R0PN2M;Database=Project;Integrated Security=True");
+        }
+        public DataTable GetVegpizza()
+        {
+            string selqry = "select Pizza_name,Pizza_price from Pizza_info where Pizza_type='Veg' ";
+            SqlDataAdapter da = new SqlDataAdapter(selqry, con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+        public DataTable getfullpizza()
+        {
+            string selqry = "select Pizza_name,Pizza_price from Pizza_info";
+            SqlDataAdapter da = new SqlDataAdapter(selqry, con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+        public DataTable GetNoNveg()
+        {
+            string selqry = "select Pizza_name,Pizza_price from Pizza_info where Pizza_type='Non Veg' ";
+            SqlDataAdapter da = new SqlDataAdapter(selqry, con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+
+        }
+        public DataTable PizzabyName(string Pizza_name)
+        {
+            String byNoQry = "select Pizza_name,Pizza_id,Pizza_price from Pizza_info where Pizza_name='" + Pizza_name+"'";
+            SqlDataAdapter da = new SqlDataAdapter(byNoQry, con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+
+        public void insert_into_order(string Pizza_name, int Pizza_id, int quantity, string crust, string toppings,string size,int price)
+        {
+            string insqry = "insert order_info(Pizza_id, Pizza_name, order_topping, order_quantity, order_crust ,order_size, order_price)values(@pid,@pname,@ptop,@pquan,@pcrust,@psize,@prce)";
+            SqlCommand cmd = new SqlCommand(insqry, con);
+            cmd.Parameters.AddWithValue("@pname", Pizza_name);
+            cmd.Parameters.AddWithValue("@pid", Pizza_id);
+            cmd.Parameters.AddWithValue("@prce", price);
+            cmd.Parameters.AddWithValue("@pquan", quantity);
+            cmd.Parameters.AddWithValue("@pcrust", crust);
+            cmd.Parameters.AddWithValue("@ptop", toppings);
+            cmd.Parameters.AddWithValue("@psize", size);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+        public DataTable Getorder()
+        {
+            string selqry = "select order_id,Pizza_name,Pizza_id,order_quantity,order_crust,order_size,order_topping,order_price from order_info";
+            SqlDataAdapter da = new SqlDataAdapter(selqry, con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+
+        }
+        public DataTable Getorderwithquanprice()
+        {
+            string selqry = "select order_id,Pizza_name,Pizza_id,order_quantity,order_crust,order_size,order_topping,(order_price*order_quantity) as price from order_info";
+            SqlDataAdapter da = new SqlDataAdapter(selqry, con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+
+        }
+        public void delete(int Pizza_id)
+        {
+            string delquery = "delete from order_info where Pizza_id=" + Pizza_id;
+            SqlCommand cmd = new SqlCommand(delquery, con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+        public void update(int order_id, string name,string address,string phno)
+        {
+            string updt = "update order_info set order_address=@ad, name_of_customer=@name,number_of_customer=@phno where order_id=@oid";
+            SqlCommand cmd = new SqlCommand(updt, con);
+            cmd.Parameters.AddWithValue("@oid", order_id);
+            cmd.Parameters.AddWithValue("@name", name);
+            cmd.Parameters.AddWithValue("@ad", address);
+            cmd.Parameters.AddWithValue("@phno", phno);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+        }
+        public DataTable gettable()
+        {
+            string selqry = "select * from order_info";
+            SqlDataAdapter da = new SqlDataAdapter(selqry, con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+
+        public void addtocrate()
+        {
+            projector pobj = new projector();
+            DataTable dt = new DataTable();
+            string selqry = "select order_id,Pizza_id,pizza_name,date_of_order,order_address,name_of_customer,number_of_customer from order_info";
+            SqlDataAdapter da = new SqlDataAdapter(selqry, con);
+            da.Fill(dt);
+            for(int i=0;i<dt.Rows.Count;i++)
+            {
+                string insqry = "insert order_cart(Pizza_id,pizza_name,order_address,name_of_customer,number_of_customer)values(@pid,@pname,@oad,@nameofc,@noofc)";
+                SqlCommand cmd = new SqlCommand(insqry, con);
+                cmd.Parameters.AddWithValue("@pid", dt.Rows[i]["Pizza_id"]);
+                cmd.Parameters.AddWithValue("@pname", dt.Rows[i]["pizza_name"]);
+                cmd.Parameters.AddWithValue("@oad", dt.Rows[i]["order_address"]);
+                cmd.Parameters.AddWithValue("@nameofc", dt.Rows[i]["name_of_customer"]);
+                cmd.Parameters.AddWithValue("@noofc", dt.Rows[i]["number_of_customer"]);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+
+        }
+        public DataTable gettablecrate()
+        {
+            string selqry = "select Pizza_id,pizza_name,order_address,date_of_order,name_of_customer,number_of_customer from order_info";
+            SqlDataAdapter da = new SqlDataAdapter(selqry, con);  
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+        public DataTable getorderhistory(string name)
+        {
+            string selqry = "select Pizza_id,pizza_name,date_of_order,name_of_customer,number_of_customer from order_cart where name_of_customer='" + name + "'";
+            SqlDataAdapter da = new SqlDataAdapter(selqry, con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+        public void truncate()
+        {
+            string trunc = "TRUNCATE TABLE order_info";
+            SqlCommand cmd = new SqlCommand(trunc, con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+    }
+}
